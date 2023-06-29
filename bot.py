505df2d83a8c75993ef0f8855ac0c194b2beef12
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-06-30 00:36:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 import re
-import datetime
 import json
+from datetime import datetime, timedelta
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
 
-
-# Initialise Bot
-'''
-updater = Updater(token='API TOKEN', use_context=True)
-dispatcher = updater.dispatcher
-'''
+# Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def load_reminders():
     try:
@@ -24,14 +22,28 @@ def load_reminders():
 reminders = load_reminders()
 
 # Saving reminders
-
 def save_reminders(reminders):
     with open('reminders.json', 'w') as f:
         json.dump(reminders, f, indent=4)
 
+# Initialise Bot
+'''
+updater = Updater(token='API TOKEN', use_context=True)
+dispatcher = updater.dispatcher
+'''
+
+# Convo states
+NAME, DATE_Q, TIME_Q, INFO, OPT, DELETE = range(6)
+UTC_1, UTC_2 = range(2)
+
+# /start command
+def start(update, context):
+    reply_markup = ReplyKeyboardMarkup([['/set', '/list'], ['/delete', '/cancel']])
+    update.message.reply_text("Hello! I'm a reminder bot created by @kairostay. What would you like to do?", reply_markup=reply_markup)
+
+
 
 # /set command
-
 def set_reminder(update, context):
     user_input = update.message.text[5:]
     pattern = r'(.+?)(.+?)(.+?)'
